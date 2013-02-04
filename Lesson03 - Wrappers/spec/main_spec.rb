71273d1a363537fb78c36a0bcef07c1d996f666c
Mark Rickert <mjar81@gmail.com>
2013-02-04 16:31:34 -0500

    AHH! Whitespace cleanup on the fly.

--- Lesson03 - Wrappers/spec/main_spec.rb
+++ Lesson03 - Wrappers/spec/main_spec.rb
@@ -11,16 +11,10 @@ end
 describe "InfoViewController" do
   tests InfoViewController
 
-
   it "changes tapped instance variable when tapped" do
-
     rotate_device :to => :landscape
-
     tap "Test Me!"
-
     controller.instance_variable_get("@viewTapped").should == true
-
+    rotate_device :to => :protrait
   end
-
-
 end
